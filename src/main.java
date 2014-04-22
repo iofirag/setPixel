9f2f4b9e340ff717f19882990a51922342723510
ofir agahi <iofirag@gmail.com>
2014-04-23 02:15:20 +0300

    paint add poligon and bizier

--- src/main.java
+++ src/main.java
@@ -1,62 +1,51 @@
-import java.awt.AWTException;
+/*This application was created by Ofir Aghai & Vidran Abdovich , 22/4/2014
+ * as part of a Graphics programming course.
+ */
+
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.MouseInfo;
+import java.awt.Dimension;
 import java.awt.Point;
-import java.awt.PointerInfo;
-import java.awt.PopupMenu;
-import java.awt.Robot;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import java.io.IOException;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.swing.JCheckBoxMenuItem;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
-import javax.swing.JRadioButtonMenuItem;
-
-
 
 public class main {
-	
+	static int numofPoints=0;
+	static List<Point> bezierPoints=new ArrayList<>();
+	static List<Point> polygonPoints=new ArrayList<>();
 	static myJPanel pane;
-	
+	static int poligon_vertex =0;
 	static Point pointPressed =null;
 	static Point pointRelease =null;
-	
 	static int lastDrag_x=0;
 	static int lastDrag_y=0;
-	
-	static int shape = 1;		// 1=Line   2=Circle   3=Rectangle
-	static int poligon_vertex=3;	//3=Triangle (default)    4=Rectangle    5=.....
+	static int shape = 1;		// 1=Line   2=Circle   3=Polygon 4=Bezier curve
 	static Color color = Color.black;
-	
-	
+
 	public static void main(String[] args) {
         final int width = 900;
-        final int height = 600;
+        final int height = 500;
 
-        
-        
         /* java window - the container managed the frame */
-        final JFrame frame = new JFrame("Direct draw demo");       
+        JFrame frame = new JFrame("Direct draw demo");       
         
         /* specific frame */
         pane = new myJPanel(width, height);
         
-        /******************************************************************
-         * Administrate window adjustment
-         */
-        /* Add the pane to the frame manager */
+        //******************************************************************
+
         frame.add(pane);
         // to show at least the panel data
         frame.pack();
@@ -64,14 +53,9 @@ public class main {
         frame.setResizable(true);     
         /* close all app in close button */
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        /**MENU's******************************************************/
-        /* set menu for window */
+
       	//Create the menu bar.
         JMenuBar menuBar = new JMenuBar();
-        
-        // Menu
-        //JMenu menu = new JMenu("Menu");
         JMenuItem menu_clearScreen = new JMenuItem("Clear Screen");
         menu_clearScreen.addActionListener(new ActionListener() {
 			@Override
@@ -79,10 +63,7 @@ public class main {
 				pane.fillCanvas(Color.white);
 			}
 		});
-        //Build the first menu.
-        //menu.add(menu_clearScrean);
-        //menu.setMnemonic(KeyEvent.VK_M);
-        //************************************************
+      
         
         // Objects Menu
         JMenu objectsMenu = new JMenu ("Objects");
@@ -109,57 +90,85 @@ public class main {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				shape=3;
-				
+
 				//custom title, custom icon
-				String result = JOptionPane.showInputDialog(frame, "Enter poligon vertex:");
+				String result = JOptionPane.showInputDialog("Enter poligon vertex:");
 				poligon_vertex= Integer.parseInt(result);
+			}
+		});
+
+        JMenuItem objectItem_bezier = new JMenuItem("Bezier Curve");
+        objectItem_bezier.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				shape=4;
 			}
 		});
         objectsMenu.add(objectItem_line);
         objectsMenu.add(objectItem_circle);
         objectsMenu.add(objectItem_poligon);
+        objectsMenu.add(objectItem_bezier);
       //************************************************
         
         // Color Menu
         JMenu colorMenu = new JMenu("Color");
-        colorMenu.setMnemonic(KeyEvent.VK_C);
-        JMenuItem colorMenu_black = new JMenuItem("Black");
+        JMenuItem colorMenu_black = new JMenuItem("");
+        colorMenu_black.setPreferredSize(new Dimension(40, 20));
+        colorMenu_black.setBackground(Color.BLACK);
         colorMenu_black.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				color = Color.BLACK;
 			}
 		});
-        JMenuItem colorMenu_green = new JMenuItem("Green");
+        JMenuItem colorMenu_green = new JMenuItem("");
+        colorMenu_green.setPreferredSize(new Dimension(40, 20));
+        colorMenu_green.setBackground(Color.GREEN);
         colorMenu_green.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				color = Color.GREEN;
 			}
 		});
-        JMenuItem colorMenu_blue = new JMenuItem("Blue");
+        JMenuItem colorMenu_blue = new JMenuItem("");
+        colorMenu_blue.setPreferredSize(new Dimension(40, 20));
+        colorMenu_blue.setBackground(Color.BLUE);
         colorMenu_blue.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				color = Color.BLUE;
 			}
 		});
-        JMenuItem colorMenu_white = new JMenuItem("White");
+        JMenuItem colorMenu_white = new JMenuItem("");
+        colorMenu_white.setPreferredSize(new Dimension(40, 20));
+        colorMenu_white.setBackground(Color.WHITE);
         colorMenu_white.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				color = Color.WHITE;
 			}
 		});
-        JMenuItem colorMenu_yellow = new JMenuItem("yellow");
+        JMenuItem colorMenu_yellow = new JMenuItem("");
+        colorMenu_yellow.setPreferredSize(new Dimension(40, 20));
+        colorMenu_yellow.setBackground(Color.YELLOW);
         colorMenu_yellow.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				color = Color.YELLOW;
 			}
 		});
+        JMenuItem colorMenu_red = new JMenuItem("");
+        colorMenu_red.setPreferredSize(new Dimension(40, 20));
+        colorMenu_red.setBackground(Color.RED);
+        colorMenu_red.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				color = Color.RED;
+			}
+		});
         //Build the first menu.
         colorMenu.add(colorMenu_black);
+        colorMenu.add(colorMenu_red);
         colorMenu.add(colorMenu_green);
         colorMenu.add(colorMenu_blue);
         colorMenu.add(colorMenu_white);
@@ -174,33 +183,10 @@ public class main {
         frame.add(menuBar, BorderLayout.NORTH);
         /**End menu's*******************************************************/
         
-        /* visibility */
+        // visibility 
         frame.setVisible(true);
-        
-        
-
-        
-        
-        /* print Mouse location */
-//        while (true){
-//	        PointerInfo a = MouseInfo.getPointerInfo();
-//	        Point b = a.getLocation();
-//	        x = (int) b.getX();
-//	        y = (int) b.getY();
-//	        System.out.print(new Date() +" --- x="+x +", y="+ y +"\n");
-//	        
-//	         Code for move programmatically the mouse 
-//	        Robot r;
-//			try {
-//				r = new Robot();
-//				r.mouseMove(x, y);
-//			} catch (AWTException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//        }
-        
-        /* Mouse Listener */
+          
+        // Mouse Listeners 
         frame.addMouseListener(new MouseListener() {
 			@Override
 			public void mouseReleased(MouseEvent e1) {
@@ -210,27 +196,33 @@ public class main {
 				// Fix point
 				pointRelease.x-=8;
 				pointRelease.y-=53;
-				
+
 				switch (shape){
-				// Draw line
+				// Draw a line
 				case 1: 
 					if (width- pointRelease.getX() >=0 && height- pointRelease.getY() >=0 )
 						pane.drawLine(color, (int)pointPressed.getX(), (int)pointRelease.getX(), (int)pointPressed.getY(), (int)pointRelease.getY());
 					else pane.drawLine(color, (int)pointPressed.getX(), (int)lastDrag_x, (int)pointPressed.getY(), (int)lastDrag_y);
 					break;
+				//Draw a circle
 				case 2:
 					pane.drawCircle(color, pointPressed.x, pointPressed.y, pointRelease.x, pointRelease.y);
 					break;
+				//Draw a polygon
 				case 3:
-					//pane.drawPoligon(color, x, y, vX, vY, vertex);
+					polygonPoints.add(new Point(pointPressed.x,pointPressed.y));
+					polygonPoints.add(new Point(pointRelease.x,pointRelease.y));
+					pane.regularPolygon(color ,polygonPoints, poligon_vertex);
+					polygonPoints.clear();
+
 					break;
-					
+
 				}
-				
+
 			}
+
 			@Override
 			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
 				System.out.println("pressed"+e.getPoint());
 				pointPressed= new Point(e.getPoint() );
 				// Fix point
@@ -239,37 +231,43 @@ public class main {
 			}
 			@Override
 			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
 				System.out.println("exited");
-				
+
 			}
 			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
+			public void mouseEntered(MouseEvent e) {				
 				System.out.println("entered");
-				
+
 			}
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
+				if ((shape==4) && (numofPoints<4)){ //shape 4 = bezier curve
+					pane.putSuperPixel(e.getX(),e.getY(),color);
+					numofPoints++;
+					bezierPoints.add(new Point(e.getX(),e.getY()));
+					if (numofPoints==4)
+					{
+						numofPoints=0;
+						pane.drawBezierCurve(color,bezierPoints);
+						bezierPoints=new ArrayList<>();
+					}
+				}
+
 				System.out.println("clicked");
-				
 			}
 		});
         
-        /* mouse motion */
+        // mouse motion 
         frame.addMouseMotionListener(new MouseMotionListener() {
-			
+
 			@Override
 			public void mouseMoved(MouseEvent e) {
-				// TODO Auto-generated method stub
 				System.out.println("moved");
 				System.out.println( "("+e.getPoint().getX()+" , "+e.getPoint().getY()+")");
 			}
-			
+
 			@Override
 			public void mouseDragged(MouseEvent e) {
-				// TODO Auto-generated method stub
 				System.out.println("dragged");
 				lastDrag_x= (int) e.getPoint().getX();
 				lastDrag_y= (int) e.getPoint().getY();
--- src/myJPanel.java
+++ src/myJPanel.java
@@ -1,224 +1,322 @@
+/*This application was created by Ofir Aghai & Vidran Abdovich , 22/4/2014
+ * as part of a Graphics programming course.
+ */
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
-import javax.swing.JPanel;
+import java.util.List;
+
 import javax.swing.*;
 
+import Jama.Matrix;
 
 public class myJPanel extends JPanel {
-
-    private BufferedImage canvas;
-
-    
+	private BufferedImage canvas;
 
 	public myJPanel(int width, int height) {
-        canvas = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        // using auto the function getPreferredSize()
-    }
-	
-    //*******override***********************************************
-    /* open the pane in the size we want */
-    public Dimension getPreferredSize() {
-        return new Dimension(canvas.getWidth(), canvas.getHeight());
-    }
-    /* draw on the pane (NEEDED)*/
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
-        g2.drawImage(canvas, null, null);
-    }
-    //********************************************************
+		canvas = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		// using auto the function getPreferredSize()
+	}
 
-    public void fillCanvas(Color c) {
-        for (int x = 0; x < canvas.getWidth(); x++) {
-            for (int y = 0; y < canvas.getHeight(); y++) {
-                putPixel(x, y, c);
-            }
-        }
-        repaint();
-    }
+	// *******override***********************************************
+	/* open the pane in the size we want */
+	public Dimension getPreferredSize() {
+		return new Dimension(canvas.getWidth(), canvas.getHeight());
+	}
 
-    public void drawLine(Color c, int x0, int x1, int y0, int y1) {
-    	System.out.println("drawLine");
-    	/****************************BEGINING OF BREZENHAM'S LINE ALGORITHM*****************/
-    	// Implement line drawing 
-    	//ablolute length end-start 
-    	int dy = y1 - y0;
-        int dx = x1 - x0;
-        
-        // sx sy	- "1" if line from top to bottom "V"
-        //      	  "-1" if line from bottom to tom "^"
-        // errp		- the level of currently error from the line (want to be little) 
-        // xp,yp	-  current x,y point
-        int sx,sy,   errp,    xp,yp;
+	/* draw on the pane (NEEDED) */
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.drawImage(canvas, null, null);
+	}
 
-        if (dx < 0){	// if line <--
-            dx = dx * -1;
-            sx = -1;
-        }
-        else{			// if line -->
-            sx = 1;
-        }
-        if (dy < 0){	// if line ^
-            dy = dy * -1;
-            sy = -1;
-        }
-        else{			// if line V
-            sy = 1;
-        }    
+	// ********************************************************
 
-        putPixel(x0, y0, c);
-        
-        if (dx > dy){						// line |________
-            errp = 2*dy - dx;
-            yp = y0;
-            for (xp=x0; xp!=x1; xp+=sx){
-                if (errp > 0){
-                    yp+=sy;
-                    errp -= dx * 2;
-                }
-                errp += dy * 2;
-                putPixel(xp, yp, c);
-            }
-        }
-        else{								// line	 |
-        	errp = 2*dx - dy;				// 		 |
-        	xp = x0;						//		 |__
-        	for (yp=y0; yp!=y1; yp+=sy){
-                if (errp > 0){
-                    xp+=sx;
-                    errp -= dy * 2;
-                }
-                errp += dx * 2;
-                putPixel(xp, yp, c);
-            }
-        }
-        /******************************END OF BRAZENHAM LINE ALGORITHM***************************/
-        repaint();
-    }
+	public void fillCanvas(Color c) {
+		for (int x = 0; x < canvas.getWidth(); x++) {
+			for (int y = 0; y < canvas.getHeight(); y++) {
+				putPixel(x, y, c);
+			}
+		}
+		repaint();
+	}
 
-    public int calculateRadius(int x0, int y0, int x1, int y1){
-    	// Length from start to end
-    	int counter=0;
-//    	/* ablolute length end-start */
-    	/****************************BEGINING OF BREZENHAM'S LINE ALGORITHM*****************/
-        int dy = y1 - y0;
-        int dx = x1 - x0;
-        
-        int sx,sy,errp,xp,yp;
+	public void drawLine(Color c, int x0, int x1, int y0, int y1) {
+		System.out.println("drawLine");
+		/**************************** BEGINING OF BREZENHAM'S LINE ALGORITHM *****************/
+		// Implement line drawing
+		// ablolute length end-start
+		int dy = y1 - y0;
+		int dx = x1 - x0;
 
-        if (dx < 0){
-            dx = dx * -1;
-            sx = -1;
-        }
-        else{
-            sx = 1;
-        }
-        if (dy < 0){
-            dy = dy * -1;
-            sy = -1;
-        }
-        else{
-            sy = 1;
-        }    
+		// sx sy - "1" if line from top to bottom "V"
+		// "-1" if line from bottom to tom "^"
+		// errp - the level of currently error from the line (want to be little)
+		// xp,yp - current x,y point
+		int sx, sy, errp, xp, yp;
 
-        counter++;
-        if (dx > dy){
-            errp = 2*dy - dx;
-            yp = y0;
-            for (xp=x0; xp!=x1; xp+=sx){
-                if (errp > 0){
-                    yp+=sy;
-                    errp -= dx * 2;
-                }
-                errp += dy * 2;
-                counter++;
-            }
-        }
-        else{
-            errp = 2*dx - dy;
-            xp = x0;
-            for (yp=y0; yp!=y1; yp+=sy){
-                if (errp > 0){
-                    xp+=sx;
-                    errp -= dy * 2;
-                }
-                errp += dx * 2;
-                counter++;
-            }
-        }
-        return counter;
-    }
-    
-	  public void drawCircle(Color c, int x0,int y0, int radiusX, int radiusY) {
-		  int radius = calculateRadius(x0,y0, radiusX, radiusY);
-		  // Implement Circle drawing
-		  int x = radius;
-		  int y = 0;
-		  int radiusError = 1-x;
-		 
-		  while(x >= y){
-			  putPixel(x + x0, y + y0, c);
-			  putPixel(y + x0, x + y0, c);
-			  putPixel(-x + x0, y + y0, c);
-			  putPixel(-y + x0, x + y0, c);
-			  putPixel(-x + x0, -y + y0, c);
-			  putPixel(-y + x0, -x + y0, c);
-			  putPixel(x + x0, -y + y0, c);
-			  putPixel(y + x0, -x + y0, c);
-			  y++;
-			  if (radiusError<0){
-				  radiusError += 2 * y + 1;
-			  }else {
-				  x--;
-				  radiusError+= 2 * (y - x + 1);
-			  }
-		  }
-		  repaint();
-	  }
+		if (dx < 0) { // if line <--
+			dx = dx * -1;
+			sx = -1;
+		} else { // if line -->
+			sx = 1;
+		}
+		if (dy < 0) { // if line ^
+			dy = dy * -1;
+			sy = -1;
+		} else { // if line V
+			sy = 1;
+		}
 
-	  
-    public void drawPoligon(Color c, int x, int y, int vX, int vY, int vertex){
-	    // Implement rectangle drawing
-	    
-	    repaint();
-    }
-	  
-	  
-//    public void drawRect(Color c, int x1, int y1, int width, int height) {
-//    int color = c.getRGB();
-//    // Implement rectangle drawing
-//    for (int x = x1; x < x1 + width; x++) {
-//        for (int y = y1; y < y1 + height; y++) {
-//            canvas.setRGB(x, y, color);
-//        }
-//    }
-//    repaint();
-//}
+		putPixel(x0, y0, c);
 
-//public void drawOval(Color c, int x1, int y1, int width, int height) {
-//    // Implement oval drawing
-//    repaint();
-//}
-	  
-	  
-    /* getter & setter */
-    public BufferedImage getCanvas() {
+		if (dx > dy) { // line |________
+			errp = 2 * dy - dx;
+			yp = y0;
+			for (xp = x0; xp != x1; xp += sx) {
+				if (errp > 0) {
+					yp += sy;
+					errp -= dx * 2;
+				}
+				errp += dy * 2;
+				putPixel(xp, yp, c);
+			}
+		} else { // line |
+			errp = 2 * dx - dy; // |
+			xp = x0; // |__
+			for (yp = y0; yp != y1; yp += sy) {
+				if (errp > 0) {
+					xp += sx;
+					errp -= dy * 2;
+				}
+				errp += dx * 2;
+				putPixel(xp, yp, c);
+			}
+		}
+		/****************************** END OF BRAZENHAM LINE ALGORITHM ***************************/
+		repaint();
+	}
+
+	public int calculateRadius(int x0, int y0, int x1, int y1) {
+		// Length from start to end
+		int counter = 0;
+		// /* ablolute length end-start */
+		/**************************** BEGINING OF BREZENHAM'S LINE ALGORITHM *****************/
+		int dy = y1 - y0;
+		int dx = x1 - x0;
+
+		int sx, sy, errp, xp, yp;
+
+		if (dx < 0) {
+			dx = dx * -1;
+			sx = -1;
+		} else {
+			sx = 1;
+		}
+		if (dy < 0) {
+			dy = dy * -1;
+			sy = -1;
+		} else {
+			sy = 1;
+		}
+
+		counter++;
+		if (dx > dy) {
+			errp = 2 * dy - dx;
+			yp = y0;
+			for (xp = x0; xp != x1; xp += sx) {
+				if (errp > 0) {
+					yp += sy;
+					errp -= dx * 2;
+				}
+				errp += dy * 2;
+				counter++;
+			}
+		} else {
+			errp = 2 * dx - dy;
+			xp = x0;
+			for (yp = y0; yp != y1; yp += sy) {
+				if (errp > 0) {
+					xp += sx;
+					errp -= dy * 2;
+				}
+				errp += dx * 2;
+				counter++;
+			}
+		}
+		return counter;
+	}
+
+	// Implementation of circle drawing algorithm
+	public void drawCircle(Color c, int x0, int y0, int radiusX, int radiusY) {
+		int radius = calculateRadius(x0, y0, radiusX, radiusY);
+		int x = radius;
+		int y = 0;
+		int radiusError = 1 - x;
+
+		while (x >= y) {
+			putPixel(x + x0, y + y0, c);
+			putPixel(y + x0, x + y0, c);
+			putPixel(-x + x0, y + y0, c);
+			putPixel(-y + x0, x + y0, c);
+			putPixel(-x + x0, -y + y0, c);
+			putPixel(-y + x0, -x + y0, c);
+			putPixel(x + x0, -y + y0, c);
+			putPixel(y + x0, -x + y0, c);
+			y++;
+			if (radiusError < 0) {
+				radiusError += 2 * y + 1;
+			} else {
+				x--;
+				radiusError += 2 * (y - x + 1);
+			}
+		}
+		repaint();
+	}
+
+	// Polygon drawing
+	public void regularPolygon(Color c, List<Point> polygonPoints,
+			int pointsNumber) {
+		Point p[] = new Point[pointsNumber];
+		int disatance = (int) Math
+				.sqrt((polygonPoints.get(0).getX() - polygonPoints.get(1)
+						.getX())
+						* (polygonPoints.get(0).getX() - polygonPoints.get(1)
+								.getX())
+						+ (polygonPoints.get(0).getY() - polygonPoints.get(1)
+								.getY())
+						* (polygonPoints.get(0).getY() - polygonPoints.get(1)
+								.getY()));
+		for (int i = 0; i < pointsNumber; i++) {
+			p[i] = new Point();
+			p[i].setLocation(
+					((int) (polygonPoints.get(0).getX() + disatance
+							* Math.cos(i * 2 * Math.PI / pointsNumber))),
+					(int) (polygonPoints.get(0).getY() + disatance
+							* Math.sin(i * 2 * Math.PI / pointsNumber)));
+		}
+		polygonPoints.clear();
+		polygonPoints.add(new Point((int) (p[0].getX()), (int) (p[0].getY())));
+		Point pointToClose = new Point(((int) (p[0].getX())),
+				(int) (p[0].getY()));
+		for (int i = 0; i < pointsNumber; i++) {
+			polygonPoints.add(new Point((int) (p[i].getX()),
+					(int) (p[i].getY())));
+			drawLine(c, polygonPoints.get(0).x, polygonPoints.get(1).x,
+					polygonPoints.get(0).y, polygonPoints.get(1).y);
+			polygonPoints.remove(0);
+		}
+		// Drawing the last line to a point the started the shape.
+		polygonPoints.add(pointToClose);
+		drawLine(c, polygonPoints.get(0).x, pointToClose.x,
+				polygonPoints.get(0).y, pointToClose.y);
+	}
+
+	// This function recieves 4 points from the user using mouse listeners and
+	// then calculates the
+	// Bezier curve and draws it on the canvas.
+	public void drawBezierCurve(Color color, List<Point> points) {
+		int pointX = 0;
+		int pointY = 0;
+		int prevX = 0;
+		int prevY = 0;
+
+		double t = 0.00; // t value from bezier curve calculation formula
+		// getting matrixes with Ax,Bx,Cx,Dx,Ay,By,Cy,Dy values
+		Matrix resultsForX = bezierFormula(points.get(0).x, points.get(1).x,
+				points.get(2).x, points.get(3).x);
+		Matrix resultsForY = bezierFormula(points.get(0).y, points.get(1).y,
+				points.get(2).y, points.get(3).y);
+
+		// calculating x and y pixel position using bezier curve formula, x(t)
+		// and y(t)
+		// this is the point we draw the line FROM
+		prevX = (int) ((resultsForX.get(0, 0) * Math.pow(t, 3))
+				+ (resultsForX.get(1, 0) * Math.pow(t, 2))
+				+ (resultsForX.get(2, 0) * t) + (resultsForX.get(3, 0)) - 8);
+		prevY = (int) ((resultsForY.get(0, 0) * Math.pow(t, 3))
+				+ (resultsForY.get(1, 0) * Math.pow(t, 2))
+				+ (resultsForY.get(2, 0) * t) + (resultsForY.get(3, 0)) - 53);
+
+		while (t <= 1.00) {
+			// calculating x and y pixel position using bezier curve formula,
+			// x(t) and y(t)
+			// this is the point we draw the line TO
+			pointX = (int) ((resultsForX.get(0, 0) * Math.pow(t, 3))
+					+ (resultsForX.get(1, 0) * Math.pow(t, 2))
+					+ (resultsForX.get(2, 0) * t) + (resultsForX.get(3, 0)) - 8);
+			pointY = (int) ((resultsForY.get(0, 0) * Math.pow(t, 3))
+					+ (resultsForY.get(1, 0) * Math.pow(t, 2))
+					+ (resultsForY.get(2, 0) * t) + (resultsForY.get(3, 0)) - 53);
+
+			drawLine(color, prevX, pointX, prevY, pointY);
+			t += 0.001;
+			// Setting current point to be the previous point. we will draw the
+			// next line starting this from this point
+			prevX = pointX;
+			prevY = pointY;
+		}
+
+		// drawing last line
+		drawLine(color, pointX, points.get(3).x - 8, pointY,
+				points.get(3).y - 53);
+	}
+
+	// This function recieves X or Y values and and multiplies them with Bezier
+	// matrix.
+	// Returns the multiplied matrix
+	public Matrix bezierFormula(double v1, double v2, double v3, double v4) {
+
+		double[][] values = { { v1, 0, 0, 0 }, { v2, 0, 0, 0 },
+				{ v3, 0, 0, 0 }, { v4, 0, 0, 0 } };
+		Matrix a = new Matrix(values);
+
+		double[][] bezierMatrix = { { -1, 3, -3, 1 }, { 3, -6, 3, 0 },
+				{ -3, 3, 0, 0 }, { 1, 0, 0, 0 } };
+		Matrix b = new Matrix(bezierMatrix);
+
+		Matrix result = b.times(a);
+		return result;
+	}
+
+	/* getter & setter */
+	public BufferedImage getCanvas() {
 		return canvas;
 	}
+
 	public void setCanvas(BufferedImage canvas) {
 		this.canvas = canvas;
 	}
 
-	public void putPixel(int x, int y, Color c){
+	public void putPixel(int x, int y, Color c) {
 		int color = c.getRGB();
-		try{
-    		canvas.setRGB(x, y, color);
-    	}catch(Exception e){
-    		System.out.println("Coordinate out of bounds!");
-    	}
+		try {
+			canvas.setRGB(x, y, color);
+		} catch (Exception e) {
+			System.out.println("Coordinate out of bounds!");
+		}
+	}
+
+	// Used to Bold up the Bezier curve points selected by the user
+	public void putSuperPixel(int x, int y, Color c) {
+		int color = c.getRGB();
+		try {
+			// fixing point accuracy
+			x -= 8;
+			y -= 53;
+			canvas.setRGB(x, y, color);
+			canvas.setRGB(x, y - 1, color);
+			canvas.setRGB(x, y - 2, color);
+			canvas.setRGB(x - 1, y - 1, color);
+			canvas.setRGB(x + 1, y - 1, color);
+		} catch (Exception e) {
+			System.out.println("Coordinate out of bounds!");
+		}
 	}
 
 }
